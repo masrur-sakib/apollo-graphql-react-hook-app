a0d5d4c4521402505228384a8d4b9d45bbd0ad79
Masrur Sakib <mashrur333@gmail.com>
2020-11-05 00:33:59 +0600

    Design added and bug fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import gql from "graphql-tag"
 import { useQuery } from "@apollo/react-hooks"
 
@@ -18,11 +19,47 @@ const query = gql`
 
 function App() {
   const {loading, data } = useQuery(query)
-  if(loading) return <h4>Data loading, Please wait.</h4>
-  console.log(data);
+  if(loading) return <h4 className="text-info">Data loading, Please wait.</h4>
   return (
     <div className="App">
-      <h1>{data.user.name}</h1>
+      <nav className="navbar navbar-light text-light site-header">
+        <div className="container-fluid">
+            <a className="navbar-brand site-title" href="/">
+                <h4>React Frontend Tasks</h4>
+            </a>
+        </div>
+      </nav>
+      <div className="apollo-graphql-task-page">
+        <nav className="navbar navbar-light text-light apollo-graphql-page-header">
+          <div className="container-fluid apollo-graphql-page-title">
+              <a className="navbar-brand" href="/apollo-graphql">
+                  <h5>Apollo Hook (GraphQL) Task</h5>
+              </a>
+          </div>
+        </nav>
+        <table className="table table-bordered">
+          <thead className="thead-light bg-info">
+              <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Username</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Phone</th>
+                  <th scope="col">Website</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td class>{data.user.id}</td>
+                  <td>{data.user.name}</td>
+                  <td>{data.user.username}</td>
+                  <td>{data.user.email}</td>
+                  <td>{data.user.phone}</td>
+                  <td>{data.user.website}</td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
